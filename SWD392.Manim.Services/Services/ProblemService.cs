64f1528fc7f9c0b13da274a01bea1b5f2013ac50
hoangleminhtriet.bonzy <hoangleminhtriet@gmail.com>
2024-10-26 16:58:35 +0700

    create problem service

--- SWD392.Manim.Services/Services/ProblemService.cs
+++ SWD392.Manim.Services/Services/ProblemService.cs
@@ -10,79 +10,80 @@ using Microsoft.EntityFrameworkCore;
 
 namespace SWD392.Manim.Services.Services
 {
-    public class ProblemService : IProblemService
-    {
-        private readonly IMapper _mapper;
-        private IUnitOfWork _unitOfWork;
-        public ProblemService(IMapper mapper, IUnitOfWork unitOfWork)
-        {
-            _mapper = mapper;
-            _unitOfWork = unitOfWork;
-        }
-        public async Task<PaginatedList<GetProblemsVM>?> GetProblems(int index, int pageSize, string? id, string? nameSearch)
-        {
-            IQueryable<Problem> query = _unitOfWork.GetRepository<Problem>().Entities.Where(s => !s.DeletedAt.HasValue);
+	public class ProblemService : IProblemService
+	{
+		private readonly IMapper _mapper;
+		private IUnitOfWork _unitOfWork;
+		public ProblemService(IMapper mapper, IUnitOfWork unitOfWork)
+		{
+			_mapper = mapper;
+			_unitOfWork = unitOfWork;
+		}
+		public async Task<PaginatedList<GetProblemsVM>?> GetProblems(int index, int pageSize, string? id, string? nameSearch)
+		{
+			IQueryable<Problem> query = _unitOfWork.GetRepository<Problem>().Entities.Where(s => !s.DeletedAt.HasValue);
 
-            if (!string.IsNullOrWhiteSpace(id))
-            {
-                query = query.Where(lp => lp.Id.ToString().Contains(id));
-            }
+			if (!string.IsNullOrWhiteSpace(id))
+			{
+				query = query.Where(lp => lp.Id.ToString().Contains(id));
+			}
 
-            if (!string.IsNullOrWhiteSpace(nameSearch))
-            {
-                query = query.Where(lp => lp.Name.Contains(nameSearch));
-            }
+			if (!string.IsNullOrWhiteSpace(nameSearch))
+			{
+				query = query.Where(lp => lp.Name.Contains(nameSearch));
+			}
 
-            var resultQuery = await _unitOfWork.GetRepository<Problem>().GetPagging(query, index, pageSize);
+			var resultQuery = await _unitOfWork.GetRepository<Problem>().GetPagging(query, index, pageSize);
 
-            var responseItems = resultQuery.Items.Select(item => _mapper.Map<GetProblemsVM>(item)).ToList();
+			var responseItems = resultQuery.Items.Select(item => _mapper.Map<GetProblemsVM>(item)).ToList();
 
-            // Create paginated response
-            var responsePaginatedList = new PaginatedList<GetProblemsVM>(
-                responseItems,
-                resultQuery.TotalCount,
-                resultQuery.PageNumber,
-                resultQuery.TotalPages
-            );
-            return responsePaginatedList;
-        }
+			// Create paginated response
+			var responsePaginatedList = new PaginatedList<GetProblemsVM>(
+				responseItems,
+				resultQuery.TotalCount,
+				resultQuery.PageNumber,
+				resultQuery.TotalPages
+			);
+			return responsePaginatedList;
+		}
 
-        public async Task<GetProblemsVM?> GetProblemById(string id)
-        {
-            Problem? existedProblem = await _unitOfWork.GetRepository<Problem>().Entities.Where(s => s.Id == id && !s.DeletedAt.HasValue).FirstOrDefaultAsync() ?? throw new ErrorException(StatusCodes.Status409Conflict, ErrorCode.Conflicted, "Vấn đề không tồn tại!");
-            return _mapper.Map<GetProblemsVM?>(existedProblem);
-        }
-        public async Task PostProblem(PostProblemVM model)
-        {
-            Problem? existedProblem = await _unitOfWork.GetRepository<Problem>().Entities.Where(s => s.Name == model.Name).FirstOrDefaultAsync();
-            if (existedProblem != null)
-            {
-                throw new ErrorException(StatusCodes.Status409Conflict, ErrorCode.Conflicted, "Tên vấn đề đã tồn tại!");
-            }
-            Problem problemType = _mapper.Map<Problem>(model);
-            problemType.CreatedAt = DateTime.Now;
+		public async Task<GetProblemsVM?> GetProblemById(string id)
+		{
+			Problem? existedProblem = await _unitOfWork.GetRepository<Problem>().Entities.Where(s => s.Id == id && !s.DeletedAt.HasValue).FirstOrDefaultAsync() ?? throw new ErrorException(StatusCodes.Status409Conflict, ErrorCode.Conflicted, "Vấn đề không tồn tại!");
+			return _mapper.Map<GetProblemsVM?>(existedProblem);
+		}
+		public async Task PostProblem(PostProblemVM model)
+		{
+			Problem? existedProblem = await _unitOfWork.GetRepository<Problem>().Entities.Where(s => s.Name == model.Name).FirstOrDefaultAsync();
+			if (existedProblem != null)
+			{
+				throw new ErrorException(StatusCodes.Status409Conflict, ErrorCode.Conflicted, "Tên vấn đề đã tồn tại!");
+			}
+			Problem problemType = _mapper.Map<Problem>(model);
+			problemType.CreatedAt = DateTime.Now;
 
-            await _unitOfWork.GetRepository<Problem>().InsertAsync(problemType);
-            await _unitOfWork.SaveAsync();
-        }
+			await _unitOfWork.GetRepository<Problem>().InsertAsync(problemType);
+			await _unitOfWork.SaveAsync();
+		}
 
-        public async Task PutProblem(string id, PostProblemVM model)
-        {
-            Problem? existedProblem = await _unitOfWork.GetRepository<Problem>().Entities.Where(s => s.Id == id && !s.DeletedAt.HasValue).FirstOrDefaultAsync() ?? throw new ErrorException(StatusCodes.Status409Conflict, ErrorCode.Conflicted, "Vấn đề không tồn tại!");
+		public async Task PutProblem(string id, PostProblemVM model)
+		{
+			Problem? existedProblem = await _unitOfWork.GetRepository<Problem>().Entities.Where(s => s.Id == id && !s.DeletedAt.HasValue).FirstOrDefaultAsync() ?? throw new ErrorException(StatusCodes.Status409Conflict, ErrorCode.Conflicted, "Vấn đề không tồn tại!");
 
-            _mapper.Map(model, existedProblem);
-            existedProblem.UpdatedAt = DateTime.Now;
-            await _unitOfWork.GetRepository<Problem>().UpdateAsync(existedProblem);
-            await _unitOfWork.SaveAsync();
-        }
-        public async Task DeleteProblem(string id)
-        {
-            Problem? existedProblem = await _unitOfWork.GetRepository<Problem>().GetByIdAsync(id) ?? throw new ErrorException(StatusCodes.Status404NotFound, ErrorCode.NotFound, "Vấn đề không tồn tại!");
-            existedProblem.DeletedAt = DateTime.Now;
-            await _unitOfWork.GetRepository<Problem>().UpdateAsync(existedProblem);
-            await _unitOfWork.SaveAsync();
-        }
+			_mapper.Map(model, existedProblem);
+			existedProblem.UpdatedAt = DateTime.Now;
+			await _unitOfWork.GetRepository<Problem>().UpdateAsync(existedProblem);
+			await _unitOfWork.SaveAsync();
+		}
+		public async Task DeleteProblem(string id)
+		{
+			Problem? existedProblem = await _unitOfWork.GetRepository<Problem>().GetByIdAsync(id) ?? throw new ErrorException(StatusCodes.Status404NotFound, ErrorCode.NotFound, "Vấn đề không tồn tại!");
+			existedProblem.DeletedAt = DateTime.Now;
+			await _unitOfWork.GetRepository<Problem>().UpdateAsync(existedProblem);
+			await _unitOfWork.SaveAsync();
+		}
 
 
-    }
+	}
 }
+
