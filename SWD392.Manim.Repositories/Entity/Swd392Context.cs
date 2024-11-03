d5a59a76a1076500273f9cf89291af1d1961fe8a
Nguyen Viet Nam Khanh <nguyenvietnamkhanhbl@gmail.com>
2024-11-04 01:12:55 +0700

    Update

--- SWD392.Manim.Repositories/Entity/Swd392Context.cs
+++ SWD392.Manim.Repositories/Entity/Swd392Context.cs
@@ -33,8 +33,8 @@ public class Swd392Context : IdentityDbContext<ApplicationUser, ApplicationRole,
     {
         //IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../SWD392.Manim.API")).AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build();
         //return configuration["ConnectionStrings:DefautDB"];
-        //return "server=manimsql.database.windows.net;database=swd-manim;uid=adminmanim;pwd=Jpassword@;TrustServerCertificate=True";
-        return "server=(local);database=swd-manim;uid=sa;pwd=12345678;Trusted_Connection=True;Trust Server Certificate=True;Timeout=30;";
+        return "server=tcp:manimsql.database.windows.net,1433;database=manim;uid=adminmanim;pwd=Jpassword@;TrustServerCertificate=True";
+        //return "server=(local);database=swd-manim;uid=sa;pwd=12345678;Trusted_Connection=True;Trust Server Certificate=True;Timeout=30;";
 
     }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(GetConnectionString());
